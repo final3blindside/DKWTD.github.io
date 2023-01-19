4ca5dbc604ff8d024414d0ab3c204acc046ec1ea
final3blindside <72025445+final3blindside@users.noreply.github.com>
2023-01-19 03:17:13 +0000

    Start: Revamp project. Removed many files.

--- registration.js
+++
@@ -1,8 +0,0 @@
-alert("You are signing up for an account in The Journal Pump app in order to access both its Free and Premium services and would require your basic information to continue, Would you like to continue?");
-
-alert(confirm("Do you agree with our Rules and Regulations and the Data Privacy Act of 2017?"));
-
-
-
-
-
